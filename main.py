2821b8196744a570e9e3b53036928c020f0b9e4c
CunningNobleman <45738611+CunningNobleman@users.noreply.github.com>
2025-05-02 16:28:10 +0300

    Trying to start
    Just adding some general structure

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from fastapi import FastAPI
+from .database import engine, Base
+from .routers import books, users, reviews, loans
 
-app = FastAPI
+app = FastAPI()
+
+# All routes
+app.include_router(users.router)
+app.include_router(books.router)
+app.include_router(reviews.router)
+app.include_router(loans.router)
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
