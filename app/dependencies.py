f576005508dcbf8af5d9498a984f353b5ce77215
CunningNobleman <45738611+CunningNobleman@users.noreply.github.com>
2025-05-03 14:50:41 +0300

    generated secret key

--- app/dependencies.py
+++ app/dependencies.py
@@ -6,7 +6,7 @@ from .models import TokenData
 from .crud.users import get_user
 from .database import get_db_connection
 
-SECRET_KEY = "your-secret-key"
+SECRET_KEY = "ba32c1bf6f5c1e9211992a687556b7038d0fa07fbf2b55180d2e09ddf25797c0b265f58081e206927c7c7a813ab522eafe3068e647059bf0774db9764523c5c988417d25cf6f6fae5a5cb7bc95b28dd212d03bafa7ca5a8741a66047cc471a883f4b36d1d63c3889945549e9ea66b673fe98c733b30b7ca3cbd4ac82f013470f"
 ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
